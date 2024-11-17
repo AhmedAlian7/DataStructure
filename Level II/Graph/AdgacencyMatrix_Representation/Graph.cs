af7e77affb08ad4c03a98fc10430fd97227d6d70
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2024-11-17 19:03:31 +0200

    Add files via upload

---
+++ Level II/Graph/AdgacencyMatrix_Representation/Graph.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdgacencyMatrix_Representation
+{
+    public class Graph<T>
+    {
+        public enum enType { Direct, Indirect};
+
+        private int[,] _vertecesMatrix;
+        private Dictionary<T, int> _vertecesIndex;
+        private int _numberOfVerteces;
+        private enType _type;
+
+        public Graph(List<T> verteces, enType type)
+        {
+            _type = type;
+            _numberOfVerteces = verteces.Count;
+            _vertecesMatrix = new int[_numberOfVerteces, _numberOfVerteces];
+            _vertecesIndex = new Dictionary<T, int>();
+            for (int i = 0; i < _numberOfVerteces; i++)
+            {
+                _vertecesIndex.Add(verteces[i], i);
+            }
+        }
+
+        public void AddEdge(T sourse, T destination, int cost = 1)
+        {
+            if (!_vertecesIndex.ContainsKey(sourse) || !_vertecesIndex.ContainsKey(destination))
+            {
+                throw new EntryPointNotFoundException("Invaliad vetreces");
+            }
+
+            _vertecesMatrix[_vertecesIndex[sourse], _vertecesIndex[destination]] = cost;
+            if (_type == enType.Indirect)
+                _vertecesMatrix[_vertecesIndex[destination], _vertecesIndex[sourse]] = cost;
+        }
+        public void RemoveEdge(T sourse, T destination)
+        {
+            if (!_vertecesIndex.ContainsKey(sourse) || !_vertecesIndex.ContainsKey(destination))
+            {
+                throw new EntryPointNotFoundException("Invaliad vetreces");
+            }
+
+            _vertecesMatrix[_vertecesIndex[sourse], _vertecesIndex[destination]] = 0;
+            if (_type == enType.Indirect)
+                _vertecesMatrix[_vertecesIndex[destination], _vertecesIndex[sourse]] = 0;
+        }
+        public void Display(string Message = "Matrix Adjacency representation:")
+        {
+            Console.WriteLine($"\n{Message}\n");
+
+            Console.Write("  ");
+            foreach (var vetrex in _vertecesIndex.Keys)
+            {
+                Console.Write(vetrex + " ");
+            }
+            Console.WriteLine();
+
+            foreach (var item in _vertecesIndex)
+            {
+                Console.Write(item.Key + " ");
+                for (int i = 0; i < _numberOfVerteces; i++)
+                {
+                    Console.Write(_vertecesMatrix[item.Value, i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool IsEdgeBetween(T sourse, T destination)
+        {
+            if (!_vertecesIndex.ContainsKey(sourse) || !_vertecesIndex.ContainsKey(destination))
+            {
+                throw new EntryPointNotFoundException("Invaliad vetreces");
+            }
+
+            return (_vertecesMatrix[_vertecesIndex[sourse], _vertecesIndex[destination]] != 0);
+        }
+        public int Indegrees(T vetrex) // coloumns
+        {
+            int count = 0;
+            for (int i = 0; i < _numberOfVerteces; i++)
+            {
+                if (_vertecesMatrix[i, _vertecesIndex[vetrex]] != 0)
+                    count++;
+            }
+            return count;
+        }
+        public int Outdegrees(T vetrex) // coloumns
+        {
+            int count = 0;
+            for (int i = 0; i < _numberOfVerteces; i++)
+            {
+                if (_vertecesMatrix[_vertecesIndex[vetrex], i] != 0)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ Level II/Graph/AdjacencyList_Representation/Graph.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdjacencyList_Representation
+{
+    public class Graph<T>
+    {
+        public enum enType { Direct, Indirect };
+
+        private Dictionary<T, List<Tuple<T, int>>> _AdjacencyList;
+        private Dictionary<T, int> _vertecesIndex;
+        private int _numberOfVerteces;
+        private enType _type;
+
+        public Graph(List<T> verteces, enType type)
+        {
+            _type = type;
+            _numberOfVerteces = verteces.Count;
+            _AdjacencyList = new Dictionary<T, List<Tuple<T, int>>>();  
+            _vertecesIndex = new Dictionary<T, int>();
+            for (int i = 0; i < _numberOfVerteces; i++)
+            {
+                _vertecesIndex.Add(verteces[i], i);
+                _AdjacencyList[verteces[i]] = new List<Tuple<T, int>>();
+            }
+        }
+
+        public void AddEdge(T sourse, T destination, int cost = 1)
+        {
+            if (!_vertecesIndex.ContainsKey(sourse) || !_vertecesIndex.ContainsKey(destination))
+            {
+                throw new EntryPointNotFoundException("Invaliad vetreces");
+            }
+
+            _AdjacencyList[sourse].Add(new Tuple<T, int>(destination, cost));
+            if (_type == enType.Indirect)
+                _AdjacencyList[destination].Add(new Tuple<T, int>(sourse, cost));
+        }
+        public void RemoveEdge(T sourse, T destination)
+        {
+            if (!_vertecesIndex.ContainsKey(sourse) || !_vertecesIndex.ContainsKey(destination))
+            {
+                throw new EntryPointNotFoundException("Invaliad vetreces");
+            }
+
+            _AdjacencyList[sourse].RemoveAll(t => EqualityComparer<T>.Default.Equals(t.Item1, destination));
+            if (_type == enType.Indirect)
+            _AdjacencyList[destination].RemoveAll(t => EqualityComparer<T>.Default.Equals(t.Item1, sourse));
+
+        }
+        public void Display(string Message = "Matrix Adjacency representation:")
+        {
+            Console.WriteLine($"\n{Message}\n");
+
+            foreach (var vetrex in _AdjacencyList)
+            {
+                Console.Write($"{vetrex.Key} -> ");
+                foreach (var edge in vetrex.Value)
+                {
+                    Console.Write($"({edge.Item1}, {edge.Item2}), ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool IsEdgeBetween(T sourse, T destination)
+        {
+            if (!_vertecesIndex.ContainsKey(sourse) || !_vertecesIndex.ContainsKey(destination))
+            {
+                throw new EntryPointNotFoundException("Invaliad vetreces");
+            }
+
+            foreach (var edge in _AdjacencyList[sourse])
+            {
+                if (EqualityComparer<T>.Default.Equals(edge.Item1, destination))
+                    return true;
+            }
+            return false;
+        }
+        public int Indegrees(T vetrex) // coloumns
+        {
+            if (!_vertecesIndex.ContainsKey(vetrex))
+                throw new EntryPointNotFoundException("Invaliad vetrex");
+
+            int count = 0;
+            foreach (var item in _AdjacencyList)
+            {
+                foreach (var edge in item.Value)
+                {
+                    if (EqualityComparer<T>.Default.Equals(edge.Item1, vetrex))
+                        count++;
+                }
+            }
+            return count;
+        }
+        public int Outdegrees(T vetrex) // coloumns
+        {
+            if (!_vertecesIndex.ContainsKey(vetrex))
+                throw new EntryPointNotFoundException("Invaliad vetrex");
+
+            return _AdjacencyList[vetrex].Count;
+        }
+    }
+}
